cf9a9537ca6256fab0f4cea00cd7d3a34ffadcf9
David Shaevel <david@davidshaevel.com>
2017-05-01 13:55:07 -0500

    CHECKPOINT 2: finish project layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg')
+  ];
 
   constructor() { }
 
