7149a60b82ce7c67e28bce83af53ebcdaa1d3ea8
David Shaevel <david@davidshaevel.com>
2017-05-14 19:18:52 -0500

    CHECKPOINT 7: cross-component communication via recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg')
