ccd8576d08ab427c0693d3e8f7a6b406f4728da0
David Shaevel <david@davidshaevel.com>
2017-05-14 20:17:20 -0500

    CHECKPOINT 9: passing ingredients from recipes to the shopping list via a service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://c1.staticflickr.com/8/7164/6552490053_45fc8a67be_b.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
